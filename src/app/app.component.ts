71e7008d21fe5b796bfeaed2218193d0d54b95ba
ryclos <loogle@hotmail.com>
2018-06-27 17:52:04 -1000

    changement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { WelcomepagePage } from './../pages/welcomepage/welcomepage';
-/* import { HomePage } from '../pages/home/home'; */
+// import { WelcomepagePage } from './../pages/welcomepage/welcomepage';
+import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = WelcomepagePage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
